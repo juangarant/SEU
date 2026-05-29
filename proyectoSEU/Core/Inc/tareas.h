2a9b46c78c87470129cc9b1e9ec18f7160c1ec5e
Steven <rasca45@gmail.com>
2026-05-29 14:57:08 +0200

    Actualizar dirección IP del servidor en tareas.h

--- proyectoSEU/Core/Inc/tareas.h
+++ proyectoSEU/Core/Inc/tareas.h
@@ -32,7 +32,7 @@
 // Orion Context IoT name
 #define IoT_NAME   	 	  "SensorSEU_70"
 #define IoT_NAME_CLONE    "SensorSEU_ZL"
-#define SERVER_IP  "158.42.180.130"
+#define SERVER_IP  "192.168.1.166"
 #define ORION_PORT  1026
 #define TIME_PORT   5000
 
